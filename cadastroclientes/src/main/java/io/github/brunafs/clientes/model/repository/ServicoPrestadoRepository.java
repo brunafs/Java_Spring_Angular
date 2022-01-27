a6ed55c83be139067b97331de51acc8ddd340677
Bruna <42774064+brunafs@users.noreply.github.com>
2022-01-26 21:56:10 -0300

    salvar via postman serviço prestado
    salvar via postman serviço prestado

---
+++ cadastroclientes/src/main/java/io/github/brunafs/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,7 @@
+package io.github.brunafs.clientes.model.repository;
+
+import io.github.brunafs.clientes.model.entity.ServicoPrestado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+}
